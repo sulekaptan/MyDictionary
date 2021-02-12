e0abe2334f8b5321072c92f3540248b2e465ef79
sulekaptan <sule.kaptan@bil.omu.edu.tr>
2021-02-12 22:33:37 +0300

    add all files

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dictionary = new MyDictionary<string>();
+            dictionary.Add("Legend");
+            dictionary.Add("Unstoppable");
+            dictionary.Add("Stronger");
+            dictionary.Add("Stay");
+            dictionary.Add("Revolution");
+            dictionary.Add("Run like a rebel");
+
+            dictionary.List();
+        }
+    }
+}
